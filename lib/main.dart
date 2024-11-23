e32dc7a4282c4202639e7f78bebd1debc40c107a
nitingadhiya <vthink.nitin@gmail.com>
2024-11-23 11:26:41 +0530

    -remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -10,23 +10,10 @@ import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-List<String> dateTimeList = [];
+
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
 FlutterLocalNotificationsPlugin();
-
-/// Function to save the list to SharedPreferences
-Future<void> saveDateTimeList() async {
-  final prefs = await SharedPreferences.getInstance();
-  await prefs.setStringList('dateTimeList', dateTimeList);
-}
-
-/// Function to load the list from SharedPreferences
-Future<void> loadDateTimeList() async {
-  final prefs = await SharedPreferences.getInstance();
-  dateTimeList = prefs.getStringList('dateTimeList') ?? [];
-  print("------${dateTimeList}");
-}
 
 Future<void> requestPermission() async {
   final bool? result = await flutterLocalNotificationsPlugin
@@ -72,13 +59,12 @@ Future<void> main() async {
 
   const InitializationSettings initializationSettings = InitializationSettings(
     iOS: initializationSettingsIOS,
-    android: null, // Omit Android settings for iOS-specific implementation
+    android: null,
   );
 
   await flutterLocalNotificationsPlugin.initialize(
     initializationSettings,
     onDidReceiveNotificationResponse: (NotificationResponse response) {
-      // Handle notification tap
       print('Notification tapped!');
     },
   );
@@ -94,7 +80,7 @@ Future<void> initializeService() async {
     'my_foreground', // id
     'MY FOREGROUND SERVICE', // title
     description: 'This channel is used for important notifications.', // description
-    importance: Importance.low, // importance must be at low or higher level
+    importance: Importance.low,
   );
 
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
@@ -114,10 +100,7 @@ Future<void> initializeService() async {
 
   await service.configure(
     androidConfiguration: AndroidConfiguration(
-      // this will be executed when app is in foreground or background in separated isolate
       onStart: onStart,
-
-      // auto start service
       autoStart: true,
       isForegroundMode: true,
 
@@ -128,20 +111,12 @@ Future<void> initializeService() async {
       foregroundServiceTypes: [AndroidForegroundType.location],
     ),
     iosConfiguration: IosConfiguration(
-      // auto start service
       autoStart: true,
-
-      // this will be executed when app is in foreground in separated isolate
       onForeground: onStart,
-
-      // you have to enable background fetch capability on xcode project
       onBackground: onIosBackground,
     ),
   );
 }
-
-// to ensure this is executed
-// run app from xcode, then from xcode menu, select Simulate Background Fetch
 
 @pragma('vm:entry-point')
 Future<bool> onIosBackground(ServiceInstance service) async {
@@ -159,17 +134,11 @@ Future<bool> onIosBackground(ServiceInstance service) async {
 
 @pragma('vm:entry-point')
 void onStart(ServiceInstance service) async {
-  // Only available for flutter 3.0.0 and later
   DartPluginRegistrant.ensureInitialized();
-
-  // For flutter prior to version 3.0.0
-  // We have to register the plugin manually
-
   SharedPreferences preferences = await SharedPreferences.getInstance();
   await preferences.setString("hello", "world");
 
   /// OPTIONAL when use custom notification
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
   if (service is AndroidServiceInstance) {
     service.on('setAsForeground').listen((event) {
@@ -185,49 +154,10 @@ void onStart(ServiceInstance service) async {
     service.stopSelf();
   });
 
-  // bring to foreground
   Timer.periodic(const Duration(seconds: 20), (timer) async {
-    if (service is AndroidServiceInstance) {
-      print("----${await service.isForegroundService()}");
-      if (await service.isForegroundService()) {
-        /// OPTIONAL for use custom notification
-        /// the notification id must be equals with AndroidConfiguration when you call configure() method.
-        flutterLocalNotificationsPlugin.show(
-          888,
-          'COOL SERVICE',
-          'Awesome ${DateTime.now()}',
-          const NotificationDetails(
-            android: AndroidNotificationDetails(
-              'my_foreground',
-              'MY FOREGROUND SERVICE',
-              icon: 'ic_bg_service_small',
-              ongoing: true,
-            ),
-          ),
-        );
-
-        // if you don't using custom notification, uncomment this
-        service.setForegroundNotificationInfo(
-          title: "My App Service",
-          content: "Updated at ${DateTime.now()}",
-        );
-      }
-    }
     requestPermission();
     showNotification();
-    await loadDateTimeList();
-    dateTimeList.add("${DateTime.now()}");
-
-    /// Save the updated list to storage
-    await saveDateTimeList();
-    await loadDateTimeList();
-
-    /// you can see this log in logcat
-    debugPrint('FLUTTER BACKGROUND SERVICE: ${DateTime.now()}');
-    debugPrint('FLUTTER BACKGROUND SERVICE: ${dateTimeList}');
     fetchBitcoinPrice();
-
-    // test using external plugin
     final deviceInfo = DeviceInfoPlugin();
     String? device;
     if (Platform.isAndroid) {
@@ -256,12 +186,10 @@ fetchBitcoinPrice() async {
     );
 
     if (response.statusCode == 200) {
-      SharedPreferences preferences = await SharedPreferences.getInstance();
-      await preferences.setString("asas", response.data.toString());
-      print("--------???------${response.data}");
+      print("------response------${response.data}");
     }
   } catch (e) {
-    print("--------???------${e}");
+    print("--------error------$e");
   }
 }
 
@@ -289,7 +217,6 @@ class _MyAppState extends State<MyApp> {
   String text = "Stop Service";
   @override
   Widget build(BuildContext context) {
-    loadDateTimeList();
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -338,64 +265,10 @@ class _MyAppState extends State<MyApp> {
                   });
                 },
               ),
-              ListView.builder(
-                itemCount: dateTimeList.length,
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: const Icon(Icons.access_time),
-                    title: Text(dateTimeList[index]),
-                  );
-                },
-              ),
-          
             ],
           ),
         ),
       ),
-    );
-  }
-}
-
-class LogView extends StatefulWidget {
-  const LogView({Key? key}) : super(key: key);
-
-  @override
-  State<LogView> createState() => _LogViewState();
-}
-
-class _LogViewState extends State<LogView> {
-  late final Timer timer;
-  List<String> logs = [];
-
-  @override
-  void initState() {
-    super.initState();
-    timer = Timer.periodic(const Duration(seconds: 1), (timer) async {
-      final SharedPreferences sp = await SharedPreferences.getInstance();
-      await sp.reload();
-      logs = sp.getStringList('log') ?? [];
-      if (mounted) {
-        setState(() {});
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    timer.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: logs.length,
-      itemBuilder: (context, index) {
-        final log = logs.elementAt(index);
-        return Text(log);
-      },
     );
   }
 }
